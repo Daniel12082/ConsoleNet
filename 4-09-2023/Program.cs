c207658c292cc072c420ac487820678f9ac949f3
daniel12082 <137439716+Daniel12082@users.noreply.github.com>
2023-09-06 10:48:57 -0500

    feat: :tada: CSharp
    Learn CSharp

---
+++ 4-09-2023/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double d = 3D;
+        d = 4d;
+        d = 3.934_001;
+        Console.WriteLine($"{d}");
+        float f = 3_0000.5F;
+        f = 5.4f;
+        Console.WriteLine($"{f}");
+        decimal myMoney = 3000.5M;
+        myMoney = 400.75M;
+        Console.WriteLine($"{myMoney}");
+        bool check = true;
+        Console.WriteLine(check ? "activo" : "inactivo");
+        Console.WriteLine(false ? "activo" : "inactivo");
+        decimal myMoney2 = 3000.56m;
+        Console.WriteLine(myMoney2.ToString("C0"));
+        }
+}
---
+++ 5-09-2023/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*ArrayList myAl = new ArrayList();
+        myAl.Add("Hello");
+        myAl.Add("World");
+        myAl.Add("!");
+        foreach (object s in myAl)
+        {
+            Console.WriteLine(s);
+        }
+        Console.WriteLine("==================================");
+        ArrayList Carnivoros = new ArrayList();
+        ArrayList Acuaticos = new ArrayList();
+        ArrayList Animales = new ArrayList();
+        Carnivoros.Add("Leon");
+        Carnivoros.Add("Tigre");
+        Carnivoros.Add("Lobo");
+        Acuaticos.Add("Tiburon");
+        Acuaticos.Add("Delfin");
+        Acuaticos.Add("Ballena");
+        Animales.AddRange(Carnivoros);
+        foreach (object s in Animales)
+        {
+            Console.WriteLine(s.ToString());
+        }
+        Console.WriteLine("==================================");
+        Animales.AddRange(Acuaticos);
+        foreach (object s in Animales)
+        {
+            Console.WriteLine(s.ToString());
+        }
+        string ? palabra;
+        int idx;
+        ArrayList Materias = new ArrayList();
+        ArrayList cursos = new ArrayList();
+        Materias.Add("Matematicas");
+        Materias.Add("Español");
+        Materias.Add("Ingles");
+        Materias.Add("Programacion");
+        cursos.Add("Octavo");
+        cursos.Add("Noveno");
+        Console.WriteLine("EScriba una materia buscar: ");
+        palabra = Console.ReadLine();
+        Console.WriteLine(Materias.Contains(palabra) ? "Si existe" : "No existe");
+        do{
+            Console.Write("Ingrese la  posicion donde desea insertar la asignatura: ");
+            idx= Convert.ToInt32(Console.ReadLine());
+        }while(idx > Materias.Count);
+        Materias.InsertRange(idx, cursos);
+        foreach (object materia in Materias)
+        {
+            Console.WriteLine(materia.ToString());
+        }*/
+        // int [] arreglo = new int[5];
+        // for (int i = 0; i < arreglo.Length; i++)
+        // {
+        //     Console.Write("Ingrese un numero: ");
+        //     arreglo[i] = Convert.ToInt32(Console.ReadLine());
+        // }
+        // foreach (int numero in arreglo)
+        // {
+        //     Console.WriteLine(numero);
+        // }
+        // ArrayList Arreglos = new ArrayList();
+        // Arreglos.Add(1);
+        // Arreglos.AddRange(arreglo);
+        // foreach (int numero in Arreglos)
+        // {
+        //     Console.WriteLine(numero);
+        // }
+        /*ArrayList Materias = new ArrayList();
+        Materias.Add("Matematicas");
+        Materias.Add("Español");
+        Materias.Add("Ingles");
+
+        string[] MyArray = (string[])Materias.ToArray(typeof(string));
+        foreach (string materia in MyArray)
+        {
+            Console.WriteLine(materia);
+        }*/
+        ArrayList Materias = new ArrayList();
+        String[] especialidad = {"Sistemas","Mecatronica","Industrial","Gestion Empresarial","Energias Renovables","Tecnologias de la Informacion"};
+        Materias.Add("Calculo");
+        Materias.Add("Español");
+        Materias.Add("Dibujo Tecnico");
+        Materias.Add(especialidad);
+        /* foreach (Object dato in Materias){
+                    Console.WriteLine(dato.GetType());
+        } /
+        foreach (Object dato in Materias){
+            if (dato.GetType() == typeof(System.String[])){
+                foreach (String dato2 in (String[])dato){
+                    Console.WriteLine(dato2);
+                }
+            } 
+            / else{
+                Console.WriteLine(dato.ToString());
+            } */
+        }
+    }
